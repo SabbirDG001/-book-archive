2c92cba3cf84f814bbc905c270fcae898c418edd
SabbirDG001 <sabbiras75ag@gmail.com>
2021-09-02 17:19:29 +0600

    second

--- js/index.js
+++ js/index.js
@@ -1,8 +1,16 @@
+document.getElementById('error').style.display = 'none';
+document.getElementById('not-found').style.display = 'none';
 const getBooks = () => {
-    const searchText = document.getElementById('input-field').value;
+    const searchField = document.getElementById('input-field');
+    const searchText = searchField.value;
+    if (searchText === '') {
+        document.getElementById('error').style.display = 'block';
+    }
     fetch(`https://openlibrary.org/search.json?q=${searchText}`)
         .then(res => res.json())
         .then(data => haveBooks(data));
+    //clear data
+    searchField.value = '';
 };
 const haveBooks = books => {
     console.log(books);
@@ -10,19 +18,24 @@ const haveBooks = books => {
     results.innerText = `${books.docs.length}`
     const container = document.getElementById('book-container');
     const booksSelf = books.docs;
+    container.textContent = '';
+    if (books.docs == 0) {
+        document.getElementById('not-found').style.display = 'block';
+    }
     booksSelf.forEach(book => {
         // console.log(book);
         const div = document.createElement('div');
-        div.classList.add('border');
-        div.classList.add('border-black');
-        div.classList.add('rounded');
-        div.classList.add('p-3');
+        div.classList.add('border', 'border-black', 'rounded', 'p-3', 'block');
         div.innerHTML = `
-        <img src="" alt="">
-            <h3>Book name:${book.title}</h3>
-            <h3>Author:${book.author_name}</h3>
-            <h3>Publisher:${book.publisher}</h3>
-            <h3>First published:${book.first_publish_year}</h3>
+        <img class="w-1/4" src=" https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" alt="">
+            <div>
+                <h3 class="text-1xl font-semibold">${book.title}</h3>
+            </div>
+            <div>
+                <h3>Author:${book.author_name}</h3>
+                <h3>Publisher:${book.publisher}</h3>
+                <h3>First published:${book.first_publish_year}</h3>
+            </div>
         `;
         container.appendChild(div);
     });
