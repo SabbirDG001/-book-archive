8f573f172c98704853c9bf610d0b82e9a2248a9d
SabbirDG001 <sabbiras75ag@gmail.com>
2021-09-02 18:20:45 +0600

    third

--- js/index.js
+++ js/index.js
@@ -5,10 +5,13 @@ const getBooks = () => {
     const searchText = searchField.value;
     if (searchText === '') {
         document.getElementById('error').style.display = 'block';
+    } else {
+        document.getElementById('error').style.display = 'none';
+        fetch(`https://openlibrary.org/search.json?q=${searchText}`)
+            .then(res => res.json())
+            .then(data => haveBooks(data));
     }
-    fetch(`https://openlibrary.org/search.json?q=${searchText}`)
-        .then(res => res.json())
-        .then(data => haveBooks(data));
+
     //clear data
     searchField.value = '';
 };
@@ -21,12 +24,13 @@ const haveBooks = books => {
     container.textContent = '';
     if (books.docs == 0) {
         document.getElementById('not-found').style.display = 'block';
-    }
-    booksSelf.forEach(book => {
-        // console.log(book);
-        const div = document.createElement('div');
-        div.classList.add('border', 'border-black', 'rounded', 'p-3', 'block');
-        div.innerHTML = `
+    } else {
+        document.getElementById('not-found').style.display = 'none';
+        booksSelf.forEach(book => {
+            // console.log(book);
+            const div = document.createElement('div');
+            div.classList.add('border', 'border-black', 'rounded', 'p-3', 'block');
+            div.innerHTML = `
         <img class="w-1/4" src=" https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" alt="">
             <div>
                 <h3 class="text-1xl font-semibold">${book.title}</h3>
@@ -37,6 +41,8 @@ const haveBooks = books => {
                 <h3>First published:${book.first_publish_year}</h3>
             </div>
         `;
-        container.appendChild(div);
-    });
+            container.appendChild(div);
+        });
+    }
+
 }
